6a27005598aa75140ef6d88d202e66c6d773bce1
Ludlulu <62658994+LudoLud@users.noreply.github.com>
2020-07-12 18:40:12 -0400

    Add files via upload

---
+++ Windows OldBit/Allemand/Menu (GER).vb
@@ -0,0 +1,16 @@
+﻿Public Class Menu__GER_
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Process.Start("\build\GER\install.bat")
+        MsgBox("Die Installation von NTVDMx64 ist im Gange.
+
+Erstellt von leecher1337")
+    End Sub
+
+    Private Sub Menu__GER__FormClosing(sender As Object, e As EventArgs) Handles MyBase.FormClosing
+        End
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        OTVDM__GER_.Show()
+    End Sub
+End Class
