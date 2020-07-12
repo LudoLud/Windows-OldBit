e86a5469be121dc20fca47745baa42339a0abac4
Ludlulu <62658994+LudoLud@users.noreply.github.com>
2020-07-12 18:39:24 -0400

    Add files via upload

---
+++ Windows OldBit/Langue.Designer.vb
@@ -0,0 +1,275 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Langue
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Langue))
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.Button12 = New System.Windows.Forms.Button()
+        Me.Button14 = New System.Windows.Forms.Button()
+        Me.Button13 = New System.Windows.Forms.Button()
+        Me.Button15 = New System.Windows.Forms.Button()
+        Me.Button16 = New System.Windows.Forms.Button()
+        Me.Button17 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button18 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(12, 12)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "简体中文"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(93, 12)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "繁體中文"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(174, 12)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 4
+        Me.Button4.Text = "Czech"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(255, 12)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 23)
+        Me.Button5.TabIndex = 5
+        Me.Button5.Text = "Español"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(12, 41)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(75, 23)
+        Me.Button6.TabIndex = 6
+        Me.Button6.Text = "Français"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(93, 41)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(75, 23)
+        Me.Button7.TabIndex = 7
+        Me.Button7.Text = "Deutsch"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(174, 41)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(75, 23)
+        Me.Button8.TabIndex = 8
+        Me.Button8.Text = "Magyar"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(255, 41)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(75, 23)
+        Me.Button9.TabIndex = 9
+        Me.Button9.Text = "Italiano"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Button10
+        '
+        Me.Button10.Location = New System.Drawing.Point(12, 69)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(75, 23)
+        Me.Button10.TabIndex = 10
+        Me.Button10.Text = "日本人"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'Button11
+        '
+        Me.Button11.Location = New System.Drawing.Point(93, 70)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(75, 23)
+        Me.Button11.TabIndex = 11
+        Me.Button11.Text = "한국어"
+        Me.Button11.UseVisualStyleBackColor = True
+        '
+        'Button12
+        '
+        Me.Button12.Location = New System.Drawing.Point(174, 69)
+        Me.Button12.Name = "Button12"
+        Me.Button12.Size = New System.Drawing.Size(75, 23)
+        Me.Button12.TabIndex = 12
+        Me.Button12.Text = "Nederlands"
+        Me.Button12.UseVisualStyleBackColor = True
+        '
+        'Button14
+        '
+        Me.Button14.Location = New System.Drawing.Point(12, 98)
+        Me.Button14.Name = "Button14"
+        Me.Button14.Size = New System.Drawing.Size(75, 23)
+        Me.Button14.TabIndex = 14
+        Me.Button14.Text = "Português"
+        Me.Button14.UseVisualStyleBackColor = True
+        '
+        'Button13
+        '
+        Me.Button13.Location = New System.Drawing.Point(255, 69)
+        Me.Button13.Name = "Button13"
+        Me.Button13.Size = New System.Drawing.Size(75, 23)
+        Me.Button13.TabIndex = 13
+        Me.Button13.Text = "Polskie"
+        Me.Button13.UseVisualStyleBackColor = True
+        '
+        'Button15
+        '
+        Me.Button15.Location = New System.Drawing.Point(93, 98)
+        Me.Button15.Name = "Button15"
+        Me.Button15.Size = New System.Drawing.Size(75, 23)
+        Me.Button15.TabIndex = 15
+        Me.Button15.Text = "русский"
+        Me.Button15.UseVisualStyleBackColor = True
+        '
+        'Button16
+        '
+        Me.Button16.Location = New System.Drawing.Point(174, 98)
+        Me.Button16.Name = "Button16"
+        Me.Button16.Size = New System.Drawing.Size(75, 23)
+        Me.Button16.TabIndex = 16
+        Me.Button16.Text = "Svenska"
+        Me.Button16.UseVisualStyleBackColor = True
+        '
+        'Button17
+        '
+        Me.Button17.Location = New System.Drawing.Point(255, 98)
+        Me.Button17.Name = "Button17"
+        Me.Button17.Size = New System.Drawing.Size(75, 23)
+        Me.Button17.TabIndex = 0
+        Me.Button17.Text = "English"
+        Me.Button17.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(70, 124)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(198, 13)
+        Me.Label1.TabIndex = 17
+        Me.Label1.Text = "Choose your operating system language."
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 3.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(26, 124)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(38, 15)
+        Me.Button1.TabIndex = 18
+        Me.Button1.Text = "Uninstall"
+        Me.Button1.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button18
+        '
+        Me.Button18.Font = New System.Drawing.Font("Microsoft Sans Serif", 3.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button18.Location = New System.Drawing.Point(274, 124)
+        Me.Button18.Name = "Button18"
+        Me.Button18.Size = New System.Drawing.Size(38, 15)
+        Me.Button18.TabIndex = 19
+        Me.Button18.Text = " Exit"
+        Me.Button18.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.Button18.UseVisualStyleBackColor = True
+        '
+        'Langue
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(342, 143)
+        Me.Controls.Add(Me.Button18)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button17)
+        Me.Controls.Add(Me.Button16)
+        Me.Controls.Add(Me.Button15)
+        Me.Controls.Add(Me.Button13)
+        Me.Controls.Add(Me.Button14)
+        Me.Controls.Add(Me.Button12)
+        Me.Controls.Add(Me.Button11)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Langue"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Windows OldBit"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents Button14 As Button
+    Friend WithEvents Button13 As Button
+    Friend WithEvents Button15 As Button
+    Friend WithEvents Button16 As Button
+    Friend WithEvents Button17 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button18 As Button
+End Class
---
+++ Windows OldBit/Langue.vb
@@ -0,0 +1,258 @@
+﻿Public Class Langue
+    'Chargement
+    Private Sub Langue_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If System.Environment.Is64BitOperatingSystem = True Then
+
+        Else
+            MsgBox("Your operating system must be 64-bit. Otherwise, some majors problems may arise.")
+            End
+        End If
+        If My.Computer.Info.OSFullName.Contains("10") Then
+
+        Else
+            If My.Computer.Info.OSFullName.Contains("8") Then
+                MsgBox("Your operating system requires Windows 10 or higher.")
+                End
+            Else
+                MsgBox("Your operating system requires Windows 10 or higher. You are using an older version and no longer supported by Microsoft. We recommend that you upgrade to Windows 10.")
+                End
+            End If
+        End If
+    End Sub
+
+    'Français
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Dim result As Integer = MessageBox.Show("Vous avez choisi ""Français"". Nous vous recommandons de sauvegarder vos données car la désinstallation de NTVDMx64 peux laissez quelque fichier restant. Continuer?", "Windows OldBit", MessageBoxButtons.YesNo)
+        If result = DialogResult.Yes Then
+            Me.Hide()
+            Menu__fr_.Show()
+        ElseIf result = DialogResult.No Then
+
+        End If
+
+
+    End Sub
+
+    'Espagnol
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Dim result As Integer = MessageBox.Show("Has elegido ""Español"". Recomendamos que haga una copia de seguridad de sus datos porque la desinstalación de NTVDMx64 puede dejar algunos archivos restantes. ¿Seguir?", "Windows OldBit", MessageBoxButtons.YesNo)
+        If result = DialogResult.Yes Then
+            Me.Hide()
+            Menu__es_.Show()
+        ElseIf result = DialogResult.No Then
+
+        End If
+
+
+    End Sub
+
+    'Allemand
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Dim result As Integer = MessageBox.Show("Sie haben ""Deutsch"" gewählt. Wir empfehlen, dass Sie Ihre Daten sichern, da bei der Deinstallation von NTVDMx64 möglicherweise einige Dateien übrig bleiben. Mach weiter?", "Windows OldBit", MessageBoxButtons.YesNo)
+        If result = DialogResult.Yes Then
+            Me.Hide()
+            Menu__GER_.Visible = True
+        ElseIf result = DialogResult.No Then
+
+        End If
+
+
+    End Sub
+
+    'Japonais
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        Dim result As Integer = MessageBox.Show("「日本語」を選択しました。 NTVDMx64をアンインストールすると一部のファイルが残る場合があるため、データをバックアップすることをお勧めします。続ける？", "Windows OldBit", MessageBoxButtons.YesNo)
+        If result = DialogResult.Yes Then
+            Me.Hide()
+            Menu__JPN_.Show()
+        ElseIf result = DialogResult.No Then
+
+        End If
+
+
+    End Sub
+
+    'Coréen
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        Dim result As Integer = MessageBox.Show("""한국어""를 선택했습니다. NTVDMx64를 제거하면 일부 파일이 남아있을 수 있으므로 데이터를 백업하는 것이 좋습니다. 계속 하시겠습니까?", "Windows OldBit", MessageBoxButtons.YesNo)
+        If result = DialogResult.Yes Then
+            Me.Hide()
+            Menu__KOR_.Show()
+        ElseIf result = DialogResult.No Then
+
+        End If
+
+
+    End Sub
+
+    'Russe
+    Private Sub Button15_Click(sender As Object, e As EventArgs) Handles Button15.Click
+        Dim result As Integer = MessageBox.Show("Вы выбрали ""русский"". Мы рекомендуем сделать резервную копию ваших данных, потому что удаление NTVDMx64 может оставить некоторые файлы. Продолжать?", "Windows OldBit", MessageBoxButtons.YesNo)
+        If result = DialogResult.Yes Then
+            Me.Hide()
+            Menu__ru_.Show()
+        ElseIf result = DialogResult.No Then
+
+        End If
+
+
+    End Sub
+
+    'Anglais
+    Private Sub Button17_Click(sender As Object, e As EventArgs) Handles Button17.Click
+        Dim result As Integer = MessageBox.Show("You have chosen ""English"". We recommend that you back up your data because uninstalling NTVDMx64 may leave some files remaining. Carry on?", "Windows OldBit", MessageBoxButtons.YesNo)
+        If result = DialogResult.Yes Then
+            Me.Hide()
+            Menu__usa_.Show()
+        ElseIf result = DialogResult.No Then
+
+        End If
+
+
+    End Sub
+
+    'Chinois simplifié
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim result As Integer = MessageBox.Show("您选择了““简体中文””。我们建议您备份数据，因为卸载NTVDMx64可能会留下一些文件。继续？", "Windows OldBit", MessageBoxButtons.YesNo)
+        If result = DialogResult.Yes Then
+            Me.Hide()
+            Menu__CHS_.Show()
+        ElseIf result = DialogResult.No Then
+
+        End If
+
+
+    End Sub
+
+    'Chinois traditionnel
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim result As Integer = MessageBox.Show("您選擇了““繁體中文””。我們建議您保存數據，因為卸載NTVDMx64可能會留下某些文件。繼續嗎？", "Windows OldBit", MessageBoxButtons.YesNo)
+        If result = DialogResult.Yes Then
+            Me.Hide()
+            Menu__CHT_.Show()
+        ElseIf result = DialogResult.No Then
+
+        End If
+
+
+    End Sub
+
+    'Italien
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        Dim result As Integer = MessageBox.Show("Hai scelto ""Italiano"". È consigliabile eseguire il backup dei dati poiché la disinstallazione di NTVDMx64 potrebbe lasciare alcuni file. Continuare?", "Windows OldBit", MessageBoxButtons.YesNo)
+        If result = DialogResult.Yes Then
+            Me.Hide()
+            Menu__it_.Show()
+
+        ElseIf result = DialogResult.No Then
+
+        End If
+
+
+    End Sub
+
+    'Hongrois
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        Dim result As Integer = MessageBox.Show("Ön a ""Magyar"" választotta. Javasoljuk, hogy készítsen biztonsági másolatot az adatokról, mert az NTVDMx64 eltávolításakor néhány fájl maradhat. Folytasd?", "Windows OldBit", MessageBoxButtons.YesNo)
+        If result = DialogResult.Yes Then
+            Me.Hide()
+            Menu__hu_.Show()
+
+        ElseIf result = DialogResult.No Then
+
+        End If
+
+
+    End Sub
+
+    'Néerlandais
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+        Dim result As Integer = MessageBox.Show("U heeft gekozen voor ""Nederlands"". We raden u aan een back-up van uw gegevens te maken, omdat bij het verwijderen van NTVDMx64 mogelijk enkele bestanden overblijven. Doorgaan?", "Windows OldBit", MessageBoxButtons.YesNo)
+        If result = DialogResult.Yes Then
+            Me.Hide()
+            Menu__nl_.Show()
+
+        ElseIf result = DialogResult.No Then
+
+        End If
+
+
+    End Sub
+
+    'Tchèque
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim result As Integer = MessageBox.Show("Vybrali jste ""Czech"". Doporučujeme zálohovat data, protože odinstalace NTVDMx64 může některé soubory ponechat. Pokračovat?", "Windows OldBit", MessageBoxButtons.YesNo)
+        If result = DialogResult.Yes Then
+            Me.Hide()
+            Menu__cs_.Show()
+
+        ElseIf result = DialogResult.No Then
+
+        End If
+
+
+    End Sub
+
+    'Suédois
+    Private Sub Button16_Click(sender As Object, e As EventArgs) Handles Button16.Click
+        Dim result As Integer = MessageBox.Show("Du har valt ""Svenska"". Vi rekommenderar att du säkerhetskopierar dina data eftersom avinstallering av NTVDMx64 kan lämna vissa filer kvar. Fortsätt?", "Windows OldBit", MessageBoxButtons.YesNo)
+        If result = DialogResult.Yes Then
+            Me.Hide()
+            Menu__sv_.Show()
+
+        ElseIf result = DialogResult.No Then
+
+        End If
+
+
+    End Sub
+
+    'Portugais
+    Private Sub Button14_Click(sender As Object, e As EventArgs) Handles Button14.Click
+        Dim result As Integer = MessageBox.Show("Você escolheu ""Português"". Recomendamos que você faça backup dos seus dados porque a desinstalação do NTVDMx64 pode deixar alguns arquivos restantes. Continue?", "Windows OldBit", MessageBoxButtons.YesNo)
+        If result = DialogResult.Yes Then
+            Me.Hide()
+            Menu__pt_.Show()
+
+        ElseIf result = DialogResult.No Then
+
+        End If
+
+
+    End Sub
+
+    'Polonais
+    Private Sub Button13_Click(sender As Object, e As EventArgs) Handles Button13.Click
+        Dim result As Integer = MessageBox.Show("Wybrałeś „Polski””. Zalecamy wykonanie kopii zapasowej danych, ponieważ odinstalowanie NTVDMx64 może pozostawić niektóre pliki. Kontynuować?", "Windows OldBit", MessageBoxButtons.YesNo)
+        If result = DialogResult.Yes Then
+            Me.Hide()
+            Menu__pl_.Show()
+
+        ElseIf result = DialogResult.No Then
+
+        End If
+
+
+    End Sub
+
+    'Désinstaller
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim result As Integer = MessageBox.Show("Do you want to uninstall NTVDMx64?
+(To uninstall WineVDM, uninstall it in the control panel.)
+
+Warning! Some files will still be present. It was therefore important to make a backup of your data.", "Windows OldBit", MessageBoxButtons.YesNo)
+        If result = DialogResult.Yes Then
+            Process.Start("\build\uninstall\uninstall.bat")
+            MsgBox("Install in process... Restart your computer after uninstalling.
+
+Warning! Some files will still be present. It was therefore important to make a backup of your data.", "Windows OldBit")
+        ElseIf result = DialogResult.No Then
+
+        End If
+    End Sub
+
+    'Boutton Quitter
+    Private Sub Button18_Click(sender As Object, e As EventArgs) Handles Button18.Click
+        End
+    End Sub
+End Class
